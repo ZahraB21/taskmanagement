ca16aa363f587bc4529e3f8d4e56fea0e98be391
Zahra <zbehfarshad@gmail.com>
2020-05-14 10:46:21 -0700

    ProjectsList component created and added to App Component

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -3,12 +3,17 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Header from "./Header";
 
+import ProjectsList from "./ProjectsList";
+
 class App extends Component {
     render() {
         return (
             <BrowserRouter>
                 <div>
                     <Header />
+                    <Switch>
+                        <Route exact path={"/"} component={ProjectsList} />
+                    </Switch>
                 </div>
             </BrowserRouter>
         );
--- resources/js/components/layout/Header.js
+++ resources/js/components/layout/Header.js
@@ -5,7 +5,7 @@ const Header = () => (
     <nav className="navbar navbar-expand-md navbar-light navbar-laravel">
         <div className="container">
             <Link className="navbar-brand" to="/">
-                Tasksman
+                Task Management
             </Link>
         </div>
     </nav>
