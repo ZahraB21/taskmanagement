46eef59d0f29eb3d409e1632cf8763725abefeef
Zahra <zbehfarshad@gmail.com>
2020-05-14 10:14:59 -0700

    ProjectController actions have been defined now

---
+++ app/Http/Controllers/ProjectController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Project;
+
+class ProjectController extends Controller
+{
+    // Getting all projects
+   public function index(){
+        $projects = Project::where('is_completed', 'false')
+                            ->orderBy('created_at', 'DESC')
+                            ->withCount(['tasks' => function ($query) {
+                                $query->where('is_completed',false);
+                            }])
+                            ->get();
+
+        return $projects->json();
+   }
+
+   // Saving a new project
+   public function store(Request $request){
+        $validatedData = $request->validate([
+            'name' => 'required',
+            'description' => 'required'
+        ]);
+
+        $project = Project::create([
+            'name' => $validatedData['name'],
+            'description' => $validatedData['description']
+        ]);
+
+        return $response->json('Project created');
+   }
+
+   // Displaying a specific project
+   public function show($id){
+        $project = Project::with(['tasks' => function ($query){
+            $query->where('is_completed', false);
+        }])->find($id);
+
+        return $project->json();
+   }
+
+   // Updating a project
+   public function markAsCompleted(Project $project){
+       $project->is_completed = true;
+       $project->update();
+
+       return response()->json('Project updated successfully');
+   }
+}
