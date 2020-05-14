b46159b83419f467197c5c1dd99eb7e36c43b818
Zahra <zbehfarshad@gmail.com>
2020-05-14 09:47:46 -0700

    Project migration now contains the tables columns

--- database/migrations/2020_05_14_163439_create_projects_table.php
+++ database/migrations/2020_05_14_163439_create_projects_table.php
@@ -15,6 +15,9 @@ class CreateProjectsTable extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $tale->text('description');
+            $table->boolean('is_completed')->default(0);
             $table->timestamps();
         });
     }
